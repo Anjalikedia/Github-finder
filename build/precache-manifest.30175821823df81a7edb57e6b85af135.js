922a3aa7513e8b1cbe2dadb73198492bdbb6bf90
unknown <anjali@appleute.de>
2022-08-18 23:09:38 +0530

    Final Push

---
+++ build/precache-manifest.30175821823df81a7edb57e6b85af135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adcd5ffe7bc6dcdfe61552ca38c331c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49537c872da0c46ad3fc",
+    "url": "/static/css/main.b50865a3.chunk.css"
+  },
+  {
+    "revision": "791d39c054cc3e452160",
+    "url": "/static/js/2.d5dce6d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d5dce6d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49537c872da0c46ad3fc",
+    "url": "/static/js/main.64edc1cf.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
